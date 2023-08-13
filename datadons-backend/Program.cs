587e336d4426a0576315d8b9a7895ef3d1780543
Jamie <92243661+Jaorow@users.noreply.github.com>
2023-08-13 22:44:07 +1200

    setup contexts and repo

--- datadons-backend/Program.cs
+++ datadons-backend/Program.cs
@@ -1,26 +1,51 @@
-var builder = WebApplication.CreateBuilder(args);
+using Models;
+using Data;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+public class Program
 {
-    Console.WriteLine("\n\n\n\nDevelopment environment is up and running!\n\n\n");
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        // Add services to the container.
+
+        builder.Services.AddControllers();
+        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        builder.Services.AddScoped<DonsDbContext>(provider =>
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DonsDbContext>()
+                .UseInMemoryDatabase("Data Source=village.db");
+            return new DonsDbContext(optionsBuilder.Options);
+        });
+
+        // TODO: convert to local sqllite db... 
+        // builder.Services.AddDbContext<DonsDbContext>(options => options.UseSqlite(builder.Configuration["WebAPIConnection"]));
+
+
+
+
+
+        var app = builder.Build();
+
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            Console.WriteLine("\n\n\n\nDevelopment environment is up and running!\n\n\n");
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseAuthorization();
+
+        app.MapControllers();
+
+        app.Run();
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
