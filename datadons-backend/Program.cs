00dd1d0b6382bade58c284abd9f2a39c78fcf1dd
Jamie <92243661+Jaorow@users.noreply.github.com>
2023-08-14 13:52:13 +1200

    Revert "setup contexts and repo"

--- datadons-backend/Program.cs
+++ datadons-backend/Program.cs
@@ -1,51 +1,26 @@
-using Models;
-using Data;
-using Microsoft.EntityFrameworkCore;
+var builder = WebApplication.CreateBuilder(args);
 
-public class Program
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
 {
-    public static void Main(string[] args)
-    {
-        var builder = WebApplication.CreateBuilder(args);
-
-        // Add services to the container.
-
-        builder.Services.AddControllers();
-        // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
-
-        builder.Services.AddScoped<DonsDbContext>(provider =>
-        {
-            var optionsBuilder = new DbContextOptionsBuilder<DonsDbContext>()
-                .UseInMemoryDatabase("Data Source=village.db");
-            return new DonsDbContext(optionsBuilder.Options);
-        });
-
-        // TODO: convert to local sqllite db... 
-        // builder.Services.AddDbContext<DonsDbContext>(options => options.UseSqlite(builder.Configuration["WebAPIConnection"]));
-
-
-
-
-
-        var app = builder.Build();
-
-
-        // Configure the HTTP request pipeline.
-        if (app.Environment.IsDevelopment())
-        {
-            Console.WriteLine("\n\n\n\nDevelopment environment is up and running!\n\n\n");
-            app.UseSwagger();
-            app.UseSwaggerUI();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseAuthorization();
-
-        app.MapControllers();
-
-        app.Run();
-    }
+    Console.WriteLine("\n\n\n\nDevelopment environment is up and running!\n\n\n");
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
