dab6ba7d95443b7bdc99b7d686ca2e0c34f91bed
eli384 <88353612+edl21@users.noreply.github.com>
2023-08-15 21:22:17 +1200

    tripcontrollers

--- datadons-backend/Data/IRepo.cs
+++ datadons-backend/Data/IRepo.cs
@@ -1,10 +1,13 @@
 using Models;
 
-namespace Data{
-    public interface IRepo{
+namespace Data
+{
+    public interface IRepo
+    {
         string AddUser(User user);
         User GetUser(long id);
         User[] GetAllUsers();
-
+        Trip GetTrip(long id);
+        Trip[] GetAllTrips();
     }
 }
