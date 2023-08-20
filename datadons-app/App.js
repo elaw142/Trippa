e5511556307713427c80d538c37289d401fcb4ff
JamesLian7 <jameslian1104@gmail.com>
2023-08-20 19:17:43 +1200

    my-god-im-him

--- datadons-app/App.js
+++ datadons-app/App.js
@@ -2,25 +2,12 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from '@expo/vector-icons';
+import HomeScreen from './HomeScreen'; // Import the screen components
+import AccountScreen from './AccountScreen';
+import AddTripScreen from './AddTripScreen';
 import { Text, View } from 'react-native';
 
 const Tab = createBottomTabNavigator();
-
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
-
-function ProfileScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Profile Screen</Text>
-    </View>
-  );
-}
 
 export default function App() {
   return (
@@ -32,20 +19,23 @@ export default function App() {
 
             if (route.name === 'Home') {
               iconName = focused ? 'home' : 'home-outline';
-            } else if (route.name === 'Profile') {
+            } else if (route.name === 'Account') {
               iconName = focused ? 'person' : 'person-outline';
+            } else if (route.name === 'AddTrip') {
+              iconName = focused ? 'add-circle' : 'add-circle-outline';
             }
 
             return <Ionicons name={iconName} size={size} color={color} />;
           },
         })}
         tabBarOptions={{
-          activeTintColor: 'blue',
-          inactiveTintColor: 'gray',
+          activeTintColor: '#357A48',
+          inactiveTintColor: 'black',
         }}
       >
         <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
+        <Tab.Screen name="AddTrip" component={AddTripScreen} />
+        <Tab.Screen name="Account" component={AccountScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
