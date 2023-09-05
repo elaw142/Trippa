c8b796ddbf592106b3917e413a941e09a22f0f4e
Jamie <92243661+Jaorow@users.noreply.github.com>
2023-09-05 22:30:49 +1200

    fix mistakes from james branch

--- datadons-app/App.js
+++ datadons-app/App.js
@@ -49,20 +49,23 @@ export default function App() {
       // Call the function to check user's login status
       checkUserLoggedIn();
     }, []);
-  useEffect(() => {
-    async function prepare() {
-      try {
-        // Pre-load fonts, make any API calls you need to do here
-        await Font.loadAsync(Entypo.font);
-        // Artificially delay for two seconds to simulate a slow loading
-        // experience. Please remove this if you copy and paste the code!
-        // await new Promise((resolve) => setTimeout(resolve, 2000));
-      } catch (e) {
-        console.warn(e);
-      } finally {
-        // Tell the application to render
-        setAppIsReady(true);
-        
+
+    useEffect(() => {
+      async function prepare() {
+        try {
+          // Pre-load fonts, make any API calls you need to do here
+          await Font.loadAsync(Entypo.font);
+          // Artificially delay for two seconds to simulate a slow loading
+          // experience. Please remove this if you copy and paste the code!
+          // await new Promise((resolve) => setTimeout(resolve, 2000));
+        } catch (e) {
+          console.warn(e);
+        } finally {
+          // Tell the application to render
+          setAppIsReady(true);
+        }
+      }
+  
       prepare();
     }, []);
 
@@ -80,11 +83,6 @@ export default function App() {
     if (!appIsReady) {
       return null;
     }
-
-    return (
-        <View
-
-  
   
   return (
     <View style={styles.container} onLayout={onLayoutRootView}>
